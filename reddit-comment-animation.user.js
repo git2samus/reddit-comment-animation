042d65c1103617b361b4213493fc7c71c35d742c
Michael Cetrulo <mail2samus@gmail.com>
2012-06-22 23:03:48 -0300

    adding Frame class + basic animation engine

--- reddit-comment-animation.user.js
+++ reddit-comment-animation.user.js
@@ -1,7 +1,20 @@
-function animation_cb(i) {
-    i %= 3;
-    console.log(i);
-    window.setTimeout(function() {
-        animation_cb(i+1);
-    }, 1e3);
+function Frame(contents, duration) {
+    this.contents = contents;
+    this.duration = duration;
 }
+
+function animate(target, frames) {
+    var frames_l = frames.length;
+
+    function _set_frame(frame_i) {
+        var frame = frames[frame_i];
+
+        target.innerHTML = frame.contents;
+        window.setTimeout(function() {
+            _set_frame((frame_i+1) % frames_l);
+        }, frame.duration);
+    };
+
+    _set_frame(0);
+};
+
